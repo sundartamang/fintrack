7ed301f0e38e384af426d272efe78abd54728532
Sundar Tamang <sundartamang6@gmail.com>
2025-11-22 23:27:07 +0545

    feat: custom style, color varaibles added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: '<router-outlet />',
+  standalone: true,
 })
-export class App {
-  protected readonly title = signal('fintrack');
-}
+export class App {}
---
+++ src/app/components/landing/landing.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.scss',
+  standalone: true,
+})
+export class Landing {}
